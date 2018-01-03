7f206ad2b94cd657b93c630714991d4cf112dea6
Anuraag Yachamaneni <ayachama@purdue.edu>
2018-01-03 00:00:44 -0500

    Set up super simple JWT authentication server

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // Load all the packages
-var express = require('express');
-var bodyParser = require('body-parser');
-var morgan    = require('morgan');
-var mongoose  = require('mongoose');
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+var bcrypt      = require('bcrypt');
 
-var jwt = require('jsonwebtoken');
-var config = require('./config');
-var User = require('./app/models/user');
+var jwt     = require('jsonwebtoken');
+var config  = require('./config');
+var User    = require('./app/models/user');
 
 var app = express();
 
@@ -25,6 +26,113 @@ app.use(morgan('dev')); //Logs requests
 app.get('/', function(req, res) {
   res.send('Hello! The API is at http://locahost:' + port + '/api')
 })
+
+//Temporary Setup route
+app.get('/setup', function(req, res) {
+
+  // create a sample user
+  var user = new User({ 
+    name: 'Anuraag Yachamaneni', 
+    password: 'password'
+  });
+
+  // save the sample user
+  user.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+
+//API Routes
+var apiRoutes = express.Router(); 
+
+apiRoutes.post('/authenticate', function(req, res) {
+
+  // find the user
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+        // if user is found and password is right
+        // create a token with only our given payload
+    // we don't want to pass in the entire user since that has the password
+    const payload = {
+      admin: user.admin 
+    };
+        var token = jwt.sign(payload, app.get('superSecret'), {
+          expiresIn : '24 hours' // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }   
+
+    }
+
+  });
+});
+
+apiRoutes.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+
+  }
+});
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function(req, res) {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});   
+
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
 
 //Start server
 app.listen(port)
