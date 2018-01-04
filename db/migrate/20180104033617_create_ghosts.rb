1895b9485a0be3768da8ec88a1d4de5846952d02
Anuraag Yachamaneni <ayachama@purdue.edu>
2018-01-03 23:43:32 -0500

    GHOST-1: Add functionality for saving and uploading ghosts

--- db/migrate/20180104033617_create_ghosts.rb
+++ db/migrate/20180104033617_create_ghosts.rb
@@ -3,10 +3,11 @@ class CreateGhosts < ActiveRecord::Migration[5.1]
     create_table :ghosts do |t|
       t.string  :name, :null => false
       t.integer :distance
-      t.string  :data
       t.string  :status
       t.integer :time
       t.integer :calories_burned
+      
+      t.json    :data
 
       t.belongs_to :users
       t.belongs_to :events
